9275baec7b4c2cfab6d2c6604289cc2173ee1731
Jess <jessica@czachor.com>
2019-11-19 12:53:57 -0500

    9.18 Completed

--- Snacktacular/Photo.swift
+++ Snacktacular/Photo.swift
@@ -15,6 +15,9 @@ class Photo {
     var postedBy: String
     var date: Date
     var documentUUID: String // universal unique identifier
+    var dictionary: [String: Any] {
+        return ["description": description, "postedBy": postedBy, "date": date]
+    }
     
     init(image: UIImage, description: String, postedBy: String, date: Date, documentUUID: String) {
         self.image = image
@@ -29,4 +32,66 @@ class Photo {
         self.init(image: UIImage(), description: "", postedBy: postedBy, date: Date(), documentUUID: "")
     }
     
+    convenience init(dictionary: [String: Any]) {
+        let title = dictionary["title"] as! String? ?? ""
+        let text = dictionary["text"] as! String? ?? ""
+        let rating = dictionary["rating"] as! Int? ?? 0
+        let reviewerUserID = dictionary["reviewerUserID"] as! String
+        let date = dictionary["date"] as! Date? ?? Date()
+        self.init(title: title, text: text, rating: rating, reviewerUserID: reviewerUserID, date: date, documentID: "")
+    }
+    
+    convenience init() {
+        let currentUserID = Auth.auth().currentUser?.email ?? "Unknown User"
+        self.init(title: "", text: "", rating: 0, reviewerUserID: currentUserID, date: Date(), documentID: "")
+    }
+    
+    func saveData(spot: Spot, completed: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        let storage = Storage.storage()
+        // convert a photo.image to a data type so it can be saved
+        guard let photoData = self.image.jpegData(compressionQuality: 0.5) else {
+            print("*** ERROR: could not convert image to data format")
+            return completed(false)
+        }
+        let uploadMetadata = StorageMetadata()
+        uploadMetadata.contentType = "image/jpeg"
+        
+        documentUUID = UUID().uuidString //generate unique ID to use for photo image's name
+        // create a red to upload storage to spot.documentID's folder.
+        let storageRef = storage.reference().child(spot.documentID).child(self.documentUUID)
+        storageRef.putData(photoData)
+        let uploadTask = storageRef.putData(photoData, metadata: uploadMetadata) { metadata, error in
+            guard error == nil else {
+                print("ANGRY! ERROR during .putData storage upload!")
+                return
+            }
+            print("HAPPY! No issue with .putData storage upload!")
+        }
+        
+        uploadTask.observe(.success) { (snapshot) in
+            // create dictionary using the data we want to save
+            let dataToSave = self.dictionary
+            // either create new doc at docUUID or updat ethe existing doc with that name.
+                let ref = db.collection("spots").document(spot.documentID).collection("photos").document(self.documentUUID)
+                ref.setData(dataToSave) { (error) in
+                    if let error = error {
+                        print("ERROR Updating document.")
+                        return completed(false)
+                    } else {
+                        print("--- Documented updated with document ID.")
+                        completed(true)
+                    }
+                }
+            }
+        
+        uploadTask.observe(.failure) { (snapshot) in
+            if let error = snapshot.error {
+                print("** ERROR: upload task for file \(self.documentUUID) failed in spit \(spot.documentID)")
+            }
+            return completed(false)
+        }
+
+    }
+
 }
