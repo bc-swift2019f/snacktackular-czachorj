107e5cf2d9e4a34fa32335c1c12142b8414feeea
Jess <jessica@czachor.com>
2019-11-19 21:46:56 -0500

    9.19 Completed

--- Snacktacular/Photos.swift
+++ Snacktacular/Photos.swift
@@ -16,4 +16,47 @@ class Photos {
     init() {
         db = Firestore.firestore()
     }
+    
+    func loadData(spot: Spot, completed: @escaping () -> ()) {
+        guard spot.documentID != "" else {
+            return
+        }
+        
+        let storage = Storage.storage()
+        
+        db.collection("spots").document(spot.documentID).collection("photos").addSnapshotListener { (QuerySnapshot, Error) in
+            guard Error == nil else {
+                print("*** ERROR: adding the snapshot listener failed.")
+                return completed()
+            }
+            self.photoArray = []
+            var loadAttempts = 0
+            let storageRef = storage.reference().child(spot.documentID)
+            for document in QuerySnapshot!.documents {
+                let photo = Photo(dictionary: document.data())
+                photo.documentUUID = document.documentID
+                self.photoArray.append(photo)
+                
+                //loading in firebase storage images
+                let photoRef = storageRef.child(photo.documentUUID)
+                photoRef.getData(maxSize: 25 * 1025 * 1025) { data, error in
+                    if let error = error {
+                        print("******* ERROR: error occurred while reading data from file ref.")
+                        loadAttempts += 1
+                        if loadAttempts >= (QuerySnapshot!.count) {
+                            return completed()
+                        }
+                    } else {
+                        let image = UIImage(data: data!)
+                        photo.image = image!
+                        loadAttempts += 1
+                        if loadAttempts >= (QuerySnapshot!.count) {
+                            return completed()
+                        }
+                    }
+                }
+            }
+            completed()
+        }
+    }
 }
--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -101,6 +101,10 @@ class SpotDetailViewController: UIViewController {
         reviews.loadData(spot: spot) {
             self.tableView.reloadData()
         }
+        
+        photos.loadData(spot: spot) {
+            self.collectionView.reloadData()
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -314,10 +318,7 @@ extension SpotDetailViewController: UINavigationControllerDelegate, UIImagePicke
         photo.image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         dismiss(animated: true) {
             photo.saveData(spot: self.spot) { (success) in
-                if success {
-                    self.photos.photoArray.append(photo)
-                    self.collectionView.reloadData()
-                }
+                
             }
         }
     }
